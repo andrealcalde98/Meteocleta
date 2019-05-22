218540fafc25f81c26ff53096d668e991c5ce117
papidre <andrealcalde2998@gmail.com>
2019-05-22 09:45:12 +0200

    reset password

---
+++ app/src/main/java/cat/copernic/meteocleta/RecoveryActivity.java
@@ -0,0 +1,13 @@
+package cat.copernic.meteocleta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recovery);
+    }
+}
